fbe357b2e2f7dd068c1e83b8d43a30b5deeabd45
Enzo-Leone17 <hozhongen@gmail.com>
2025-03-25 22:43:27 +0800

    Added develop branch and code touchup for exercise on 20_03_2025

--- Module_1/Exercises/20_03_2025/index.js
+++ Module_1/Exercises/20_03_2025/index.js
@@ -1,8 +1,10 @@
-function show_greetings(){
-    let clientUsername = document.getElementById("username").value;
-    if(clientUsername !== "")
-    {
-        document.getElementById("greetings").innerHTML = "Hello " + clientUsername + 
-        ", nice to meet you!";
-    }
+function show_greetings() {
+  let clientUsername = document.getElementById("username").value;
+  if (clientUsername !== "") {
+    document.getElementById("greetings").innerHTML =
+      "Hello " + clientUsername + ", nice to meet you!";
+  } else {
+    document.getElementById("greetings").innerHTML = "";
+    alert("Please enter a valid name!");
+  }
 }
