a6dd1594f7e670c769e77a1fe4e4028c87868777
Enzo_Leone17 <hozhongen@gmail.com>
2025-05-28 16:46:25 +0800

    Commit and push completed Module 3 Lab Exercise Advanced js

--- Module_3/LabExercises/advanced_JavaScript/advanced.js
+++ Module_3/LabExercises/advanced_JavaScript/advanced.js
@@ -302,5 +302,38 @@ function validateStringArg(fn) {
   };
 }
 
-console.log(validatedOrderItem("Apple Watch","AirPods","Macbook","iPhone","iPad")); // should run the function
+console.log(
+  validatedOrderItem("Apple Watch", "AirPods", "Macbook", "iPhone", "iPad")
+); // should run the function
 console.log(validatedOrderItem("iPhone pro max", 123)); // should throw an error
+
+/*We can delay execution of a function using setTimeout, where we need to provide both
+the callback function and the delay after which it should execute.
+a) Create a promise-based alternative randomDelay() that delays execution for a
+random amount of time (between 1 and 20 seconds) and returns a promise we can use
+via .then(), as in the starter code below
+b) If the random delay is even, consider this a successful delay and resolve the promise,
+and if the random number is odd, consider this a failure and reject it
+c) Update the testing code to catch rejected promises and print a different message
+d) Try to update the then and catch messages to include the random delay value*/
+const randomDelay = async () => {
+  let random = Math.floor(Math.random() * 20) + 1;
+  try {
+    if (random % 2 === 0) {
+      await new Promise((resolve) => setTimeout(resolve, random * 1000));
+      return random;
+    } else {
+      return await new Promise((resolve, reject) =>
+        setTimeout(reject, random * 1000)
+      );
+    }
+  } catch (error) {
+    console.log(`There appears to have been a delay of odd: ${random} seconds.`);
+  }
+};
+randomDelay().then((delay) => {
+  delay !== undefined ? console.log(`There appears to have been a delay of even: ${delay} seconds.`) : null;
+});
+
+
+
