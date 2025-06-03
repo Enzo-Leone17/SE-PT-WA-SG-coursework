6374cb2d29ebe8db633464cbaf0610dab04020a3
Enzo-Leone17 <hozhongen@gmail.com>
2025-06-03 23:09:19 +0800

    Added dotenv and variables to avoid pushing sensitive data to repository

--- Module_5/Exercises/SequelizePractices/school/config/database.js
+++ Module_5/Exercises/SequelizePractices/school/config/database.js
@@ -1,8 +1,11 @@
 // Setting up the database connection
 const { Sequelize } = require('sequelize');
 
+//using env to store sensitive information
+require('dotenv').config();
+
 //#region Sensitive information
-const sequelize = new Sequelize('university', 'username', 'password', {
+const sequelize = new Sequelize('university', process.env.admin, process.env.admin_password, {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
